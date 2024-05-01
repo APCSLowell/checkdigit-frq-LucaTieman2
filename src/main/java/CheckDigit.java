261f6e3809fab37dd886df2021299f368707e195
LucaTieman2 <143014925+LucaTieman2@users.noreply.github.com>
2024-05-01 09:50:16 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -7,7 +7,7 @@ public class CheckDigit
    */  
    public static int getCheck(int num) 
    {  
-     int count = 0;
+     int sum = 0;
      for (int i = 1; i < getNumberOfDigits(num); i++) {
        sum += (8 - i) * getDigit(num, i);
      }
